16c4ed403077f109a422583df5e1dac415098bdb
林庭楷 <hellolinpeter@gmail.com>
2024-08-08 00:41:25 +0800

    MapFindUpperBoundIdx

--- StreamReassembler/streamReassembler.go
+++ StreamReassembler/streamReassembler.go
@@ -2,11 +2,12 @@ package StreamReassembler
 
 import (
 	"lab/stream"
+	"log"
 	"sort"
 )
 
 type StreamReassemblerInterface interface {
-	PushSubString(string, uint64, bool)
+	PushSubString(string, int, bool)
 	StreamOut() stream.Stream
 	UnassembledBytes() int
 	Empty() bool
@@ -34,11 +35,23 @@ func New(next, num, eof int, output stream.Stream, capacity int) *StreamReassemb
 	}
 }
 
-func (sr *StreamReassembler) PushSubString(data string, idx uint64, eof bool) {}
-func (sr *StreamReassembler) StreamOut() stream.Stream                        { return stream.Stream{} }
-func (sr *StreamReassembler) UnassembledBytes() int                           { return sr.unassebledBytesNum }
-func (sr *StreamReassembler) Empty() bool                                     { return sr.unassebledBytesNum == 0 }
+func (sr *StreamReassembler) PushSubString(data string, idx int, eof bool) {
+	pos, f := MapFindUpperBoundIdx(sr.unassembleStrs, idx)
+	if !f {
+		log.Panicf("Error to find the target upperbound :%d\n", idx)
+	}
+	if pos != 0 {
+		pos -= 1
+	}
+
+}
+func (sr *StreamReassembler) StreamOut() stream.Stream { return stream.Stream{} }
+func (sr *StreamReassembler) UnassembledBytes() int    { return sr.unassebledBytesNum }
+func (sr *StreamReassembler) Empty() bool              { return sr.unassebledBytesNum == 0 }
 func MapFindUpperBoundIdx(target map[int]string, idx int) (int, bool) {
+	if idx < 0 {
+		return -1, false
+	}
 	var tmp = make([]int, 0, len(target))
 	for i, _ := range target {
 		tmp = append(tmp, i)
