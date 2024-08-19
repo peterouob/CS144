f1424fedf5114ea658d5c79b662c0f8c3a6c4777
林庭楷 <hellolinpeter@gmail.com>
2024-08-19 20:31:45 +0800

    tcp reciver

--- tcpReceiver/tcpReciver.go
+++ tcpReceiver/tcpReciver.go
@@ -1,7 +1,9 @@
 package tcpReceiver
 
 import (
+	"lab/stream"
 	"lab/streamReassembler"
+	"lab/tcp_helper"
 	"lab/wrapping"
 )
 
@@ -16,3 +18,15 @@ type TcpReceiver struct {
 }
 
 var _ TcpReceiverInterface = (*TcpReceiver)(nil)
+
+func (rcv *TcpReceiver) Ackno() wrapping.WrappingInt32 {}
+
+func (rcv *TcpReceiver) WindoSize() int {}
+
+func (rcv *TcpReceiver) UnassembledBytes() int {}
+
+func (rcv *TcpReceiver) SegmentReceived(seg tcp_helper.TCPSegment) {}
+
+func (rcv *TcpReceiver) SegmentOut() stream.Stream {
+	return rcv.reassembler.StreamOut()
+}
